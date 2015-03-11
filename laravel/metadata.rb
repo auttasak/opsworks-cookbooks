a7be84a40c302e009fafeb825a6e7812d19a8875
Auttasak Wongkitta <auttasak.wongkitta@gmail.com>
2015-03-12 00:34:15 +0700

    OK

---
+++ laravel/metadata.rb
@@ -0,0 +1,7 @@
+name        "laravel"
+description "Deploy applications"
+maintainer  "AWS OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
+
+recipe "laravel::permission", "Deploy a PHP application"
