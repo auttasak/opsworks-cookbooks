cc976f41f11e1ac00b20a85c3806be4f95b10f67
Auttasak Wongkitta <auttasak.wongkitta@gmail.com>
2015-03-18 17:28:28 +0700

    edit permission

--- laravel/recipes/permission.rb
+++ laravel/recipes/permission.rb
@@ -1,1 +1,14 @@
-Chef::Log.debug("Skipping")
+[release_path].each do |path|
+  log "permission hook [#{path}]" do
+      level :debug
+  end
+  # correct permissions to allow apache to write
+  execute "chown #{path}/app/storage" do
+      cwd "#{path}/app/storage"
+      command "chown -R deploy.www-data ."
+  end
+  execute "chmod #{path}/app/storage" do
+      cwd "#{path}/app/storage"
+      command "chmod -R u+rwX,g+rwX ."
+  end
+end
