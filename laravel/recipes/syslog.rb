85702e1cac22c2fb4eafbda93827e00aecf928d8
Auttasak Wongkitta <auttasak.wongkitta@gmail.com>
2015-03-20 00:50:48 +0700

    ok

--- laravel/recipes/syslog.rb
+++ laravel/recipes/syslog.rb
@@ -2,7 +2,7 @@
 
 Chef::Log.debug("Configure remote Log ")
 template "/etc/rsyslog.d/99-centralized-log.conf" do
-  source    "99-centralized-log.erb"
+  source    "99-centralized-log.conf.erb"
   owner     'root'
   group     'root'
   mode      '0644'
