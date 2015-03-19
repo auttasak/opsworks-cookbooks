c6cfd3c08d1a0809ff1e8f4af5d685c2dd1b82d2
Auttasak Wongkitta <auttasak.wongkitta@gmail.com>
2015-03-20 00:34:48 +0700

    ok

--- laravel/recipes/syslog.rb
+++ laravel/recipes/syslog.rb
@@ -6,7 +6,6 @@ template "/etc/rsyslog.d/99-centralized-log.conf" do
   owner     'root'
   group     'root'
   mode      '0644'
-  notifies  :restart, "service[rsyslog]"
   action	:create_if_missing
 end
 
